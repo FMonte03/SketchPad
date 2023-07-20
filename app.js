97fa4020724b588823b4c08236409b9eb80f95ef
Filippo <122409227+FMonte03@users.noreply.github.com>
2023-07-20 00:40:17 -0400

    Finally got the  makeGrid function to work as intended

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const gridContainer  = document.querySelector('.gridContainer');
 const defaultSize = 800;
 const defaultSquareSize = "14px";
 
+function clearGrid(){
+    while(gridContainer.firstChild){
 
+        gridContainer.removeChild(gridContainer.firstChild); 
+    }
+
+}
 
 
 
@@ -12,11 +18,11 @@ const defaultSquareSize = "14px";
 
 let currentColor = 'black'
 
-function makeGrid(defaultSize, defaultSquareSize){
-    for(let i = 0; i< defaultSize; i++){
+function makeGrid(gridSize, squareSize){
+    for(let i = 0; i< gridSize; i++){
         const gridSquare = document.createElement('div'); 
         gridSquare.classList.add("gridSquare"); 
-        gridSquare.setAttribute('style', `height: ${defaultSquareSize}; width: ${defaultSquareSize};`);
+        gridSquare.setAttribute('style', `height: ${squareSize}px; width: ${squareSize}px;`);
         gridSquare.addEventListener('mouseover' ,() => {changeSquareColor(gridSquare)});
         gridContainer.append(gridSquare);
 
@@ -24,18 +30,52 @@ function makeGrid(defaultSize, defaultSquareSize){
     }
 }
 
+/*
+'1' : [200, 32]  ,
+'2' : [512, 20]  ,
+'3' : [800, 16]  ,
+'4' : [2048, 10]  
+*/
+
+
+const valueMap = {
+    '1' : [200, 32] ,
+    '2' : [512, 20]  ,
+    '3' : [800, 16]  ,
+    '4' : [2048, 10]
+
+}; 
+
+
+
+const pixelMap = {
+
+    '1' : '10 x 20' ,
+    '2' : '16 x 32'  ,
+    '3' : '20 x 40'  ,
+    '4' : '32 x 64'  
+
+
+}; 
+
+
 const slider = document.querySelector('.slider');
 slider.addEventListener('change', showPixels); 
 
 function showPixels(){
 
 const p = document.querySelector('#sliderValue'); 
-p.innerHTML = `${slider.value}`;
+p.innerHTML = `${pixelMap[`${slider.value}`]}`;
 
 
 
 }
-makeGrid(defaultSize, defaultSquareSize); 
+
+
+
+
+
+makeGrid(2048, 8); 
 
 function changeSquareColor(elem){
 
